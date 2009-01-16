f927b2b1d2dbd0344aa62b2a431eebc9f1aa6902
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2009-01-16 21:04:43 +0000

    added quotes for block delimiters
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@8 edfdf9a5-49d8-4bc8-8513-45a678140012

--- shell/src/main/java/org/vfsutils/shell/CommandParser.java
+++ shell/src/main/java/org/vfsutils/shell/CommandParser.java
@@ -73,8 +73,8 @@ public class CommandParser {
 	}
 	
 	/**
-	 * Splits a line in tokens at whitespace boundaries. Whitespace can be escaped by a backslash to
-	 * avoid a split.
+	 * Splits a line in tokens at whitespace boundaries. Whitespace can be escaped by a backslash and 
+	 * quoted blocks (single or double quotes) to avoid a split.
 	 * @param line
 	 * @return the tokens after the split at whitespace
 	 */
@@ -86,35 +86,71 @@ public class CommandParser {
 		
 		StringBuffer part = new StringBuffer();
 		
+		boolean escaped = false;
+		boolean inBlock = false;
+		char blockChar = '0';
+		
+		char c;
 		for (int i=0; i < chars.length; i++) {
-			char c = chars[i];
+			c = chars[i];
+			//reset escape
+			escaped = false;
 			
+			//if escaped, skip escape character
 			if (c=='\\') {
-				//when escaped whitespace parse next character
-				if (i+1<chars.length && (chars[i+1]==' ' || chars[i+1]=='\n')) {
-					char c2 = chars[++i];
-					part.append(c2);
+				escaped = true;
+				//skip to next
+				i++;
+				//stop if at end
+				if (i>=chars.length) break;
+				//reassign c
+				c = chars[i];
+			}
+			
+			if (c=='"' || c=='\'') {
+				if (escaped) {
+					part.append(c);
+				}
+				else if (inBlock && blockChar==c) {
+					inBlock = false;
+				}
+				else if (inBlock) {
+					part.append(c);
 				}
 				else {
-					part.append(c);
+					inBlock=true;
+					blockChar=c;
 				}
 			}
 			else if (c==' ' || c=='\n') {
-				//when space (and not escaped) split
-				parts.add(part.toString());
-				part.delete(0, part.length());
+				if (escaped || inBlock) {
+					part.append(c);
+				}
+				else {
+					//when space (and not escaped) split
+					addPart(parts, part);
+				}
 			}
 			else {
 				part.append(c);
 			}
 		}
 		
+		//treat left overs
 		if (part.length()>0) {
-			parts.add(part.toString());
+			addPart(parts, part);
 		}
 		
 		String[] result = new String[parts.size()];
 		return (String[]) parts.toArray(result);
 	}
 
+	private void addPart(List parts, StringBuffer part) {
+		
+		if (part.length()==0) return;
+		
+		parts.add(part.toString());
+		part.delete(0, part.length());
+	}
+	
 }
