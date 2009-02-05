447d6ed9756843b86824ff77bbda92e89f542271
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2009-02-05 20:18:43 +0000

    added simple echo command
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@20 edfdf9a5-49d8-4bc8-8513-45a678140012

---
+++ shell/src/main/java/org/vfsutils/shell/commands/Echo.java
@@ -0,0 +1,24 @@
+package org.vfsutils.shell.commands;
+
+import org.apache.commons.vfs.FileSystemException;
+import org.vfsutils.shell.Arguments;
+import org.vfsutils.shell.CommandInfo;
+import org.vfsutils.shell.Engine;
+
+public class Echo extends AbstractCommand {
+
+	public Echo() {
+		super("echo", new CommandInfo("Writes all arguments to output", "<expression>"));
+	}
+	
+	public void execute(Arguments args, Engine engine)
+			throws IllegalArgumentException, FileSystemException {
+		
+		echo(args, engine);
+	}
+	
+	public void echo(Arguments args, Engine engine) throws FileSystemException {
+		engine.println(args.asString(1));
+	}
+
+}
