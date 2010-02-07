dc236a3ea8ba947eb94244c06e992825e87f85c5
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2010-02-07 16:19:49 +0000

    allow to continue on failure
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@164 edfdf9a5-49d8-4bc8-8513-45a678140012

--- shell/core/src/main/java/org/vfsutils/shell/commands/Rm.java
+++ shell/core/src/main/java/org/vfsutils/shell/commands/Rm.java
@@ -15,12 +15,13 @@ public class Rm extends AbstractCommand implements CommandProvider {
 	public class RmOptions {
 		protected boolean verbose = false;
 		protected boolean dryRun = false;
+		protected boolean fail = true;
 		protected int cntFiles = 0;
 		protected int cntDirs = 0;
 	}
 	
 	public Rm() {
-		super("rm", new CommandInfo("Remove a file", "[-v] <path> [--dry-run]"));
+		super("rm", new CommandInfo("Remove a file", "[-vF] <path> [--dry-run]"));
 	}
 
 	public void execute(Arguments args, Engine engine)
@@ -34,47 +35,101 @@ public class Rm extends AbstractCommand implements CommandProvider {
 
 		options.verbose = args.hasFlag('v');
 		options.dryRun = args.hasFlag("dry-run");
+		options.fail = !args.hasFlag('F');
 		
-		if (path.equals("/**/*.*")) {
-			throw new IllegalArgumentException("Removing all files is disabled");
+		if (path.startsWith("/*")) {
+			throw new IllegalArgumentException("Removing all files from the root is disabled");
 		}
 		
-		final FileObject[] files = engine.pathToFiles(path);
-		
-		rm(files, options, engine);
+		try {
+			final FileObject[] files = engine.pathToFiles(path, true);
+			rm(files, options, engine);
+		}
+		catch (IllegalArgumentException e) {
+			handleException(e, options, engine);
+		}
 		
 		engine.println((options.dryRun?"[Dry run] ":"") + "Removed " + options.cntDirs + " Folder(s), " + options.cntFiles + " File(s)");
 
 	}
 	
 	public void rm(FileObject[] files, RmOptions options, Engine engine) throws FileSystemException, CommandException {
-		//iterate from the back to be able to remove folders
-		for (int i = files.length-1; i >=0; i--) {
+
+		for (int i=0; i< files.length; i++) {
 			FileObject file = files[i];
-			rm(file, options, engine);
+			try {
+				rm(file, options, engine);
+			}
+			catch (FileSystemException e) {
+				handleException(e, options, engine);
+			}
+			catch (CommandException e) {
+				handleException(e, options, engine);
+			}
 		}
 	}
 	
+	
 	public void rm(FileObject file, RmOptions options, Engine engine) throws FileSystemException, CommandException {
 		
-		boolean isFile = file.getType().equals(FileType.FILE);
+		if (!file.exists()) {
+			fail("Could not delete " + engine.toString(file) + " because it does not exist", options, engine);
+			return;
+		}
 		
-		if (!options.dryRun) {
-			if (file.delete(Selectors.SELECT_SELF)>0) {
-				if (isFile) {
-					options.cntFiles++;
-				}
-				else {
-					options.cntDirs++;
-				}
-			}
-			else {
-				throw new CommandException("Could not delete " + engine.toString(file));
+		boolean isFile = file.getType().equals(FileType.FILE);
+		boolean isDeleted = true;
+		
+		if (!options.dryRun) {			
+			isDeleted = file.delete(Selectors.SELECT_SELF)>0;
+			if (!isDeleted) {
+				fail("Could not delete " + engine.toString(file), options, engine);
 			}
 		}
+		
+		if (isDeleted) {
+			if (isFile) {
+				options.cntFiles++;
+			}
+			else {
+				options.cntDirs++;
+			}
         
-        if (options.verbose) {
-        	engine.println("Removed " + engine.toString(file));
+			verbose("Removed " + engine.toString(file), options, engine);
+		}
+        
+	}
+	
+	protected void handleException(Exception e, RmOptions options, Engine engine) throws IllegalArgumentException, CommandException {
+		if (options.fail) {
+			if (e instanceof IllegalArgumentException) {
+				throw (IllegalArgumentException) e;
+			}
+			else if (e instanceof CommandException) {
+				throw (CommandException) e;
+			}
+			else {
+				throw new CommandException(e);
+			}
+		}
+		else {
+			verbose(e.getMessage(), options, engine);
+		}
+	}
+	
+	protected void fail(String message, RmOptions options, Engine engine) throws CommandException {
+		if (options.fail) {
+			throw new CommandException(message);
+		}
+		else  {
+			verbose(message, options, engine);
+			return;
+		}
+	}
+	
+	protected void verbose(String message, RmOptions options, Engine engine) {
+		if (options.verbose) {
+        	engine.println(message);
         }
 	}
 	
