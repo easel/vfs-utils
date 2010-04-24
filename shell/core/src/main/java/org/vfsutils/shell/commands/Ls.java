a4417619ec4d663d2d743ef8b658acb111c27732
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2010-04-24 16:07:56 +0000

    small improvements
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@181 edfdf9a5-49d8-4bc8-8513-45a678140012

--- shell/core/src/main/java/org/vfsutils/shell/commands/Ls.java
+++ shell/core/src/main/java/org/vfsutils/shell/commands/Ls.java
@@ -16,40 +16,48 @@ import org.vfsutils.shell.Engine;
 public class Ls extends AbstractCommand implements CommandProvider {
 
 	private String longListType = "nix";
-	//Note that DateFormat.format is not threadsafe and so we can't store an instance
+	// Note that DateFormat.format is not threadsafe and so we can't store an
+	// instance
 	private String dateFormat = "dd/MM/yyyy HH:mm";
-	
-	
+
 	public Ls() {
 		super("ls", new CommandInfo("Directory listing", "[-l] [<path>]"));
 	}
-	
+
 	public void execute(Arguments args, Engine engine)
-			throws IllegalArgumentException, CommandException, FileSystemException {
+			throws IllegalArgumentException, CommandException,
+			FileSystemException {
 
 		FileObject file = null;
-		FileObject[] files;
-		boolean showRelativePath = false;
-
+		
+		boolean longList = args.hasFlag("l");
+		
 		if (args.size() > 0) {
 			String path = args.getArgument(0);
 			if (path.indexOf('*') > -1) {
 				file = engine.getCwd();
-				files = engine.pathToFiles(path);
-				showRelativePath = true;
+				FileObject[] files = engine.pathToFiles(path);
+
+				// List the contents
+				listChildren(file, files, true, longList, engine);
 			} else {
 				file = engine.pathToFile(path);
-				// can throw a FileNotFolderException
-				files = file.getChildren();
+				if (file.getType().hasChildren()) {
+					// can throw a FileNotFolderException
+					FileObject[] files = file.getChildren();
+					// List the contents
+					listChildren(file, files, false, longList, engine);
+				} else {
+					listChild(engine.getCwd(), file, true, false, longList, engine);
+				}
 			}
 		} else {
 			file = engine.getCwd();
 			// can throw a FileNotFolderException
-			files = file.getChildren();
+			FileObject[] files = file.getChildren();
+			// List the contents
+			listChildren(file, files, false, longList, engine);
 		}
-
-		// List the contents
-		listChildren(file, files, showRelativePath, args, engine);
 
 	}
 
@@ -57,31 +65,19 @@ public class Ls extends AbstractCommand implements CommandProvider {
 	 * Lists the given files
 	 */
 	private void listChildren(final FileObject dir, final FileObject[] files,
-			boolean showRelativePath, Arguments args, Engine engine) throws FileSystemException {
+			boolean showRelativePath, boolean longList, Engine engine)
+			throws FileSystemException {
 
 		engine.println("Contents of " + engine.toString(dir));
-
-		boolean longList = args.hasFlag("l");
-				
+		
 		int nrOfFiles = 0;
 		int nrOfFolders = 0;
 		for (int i = 0; i < files.length; i++) {
 			final FileObject child = files[i];
-			
+
 			boolean isFolder = (child.getType() == FileType.FOLDER);
-			
-			if (longList && longListType!= null && longListType.equals("dos")) {
-				SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
-				listChildDosStyle(dir, child, isFolder, showRelativePath, dateFormatter, engine);
-			}
-			else if (longList) {
-				SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
-				listChildNixStyle(dir, child, isFolder, showRelativePath, dateFormatter, engine);
-			}
-			else {				
-				listChild(dir, child, isFolder, showRelativePath, engine);
-			}
-		
+			listChild(dir, child, showRelativePath, isFolder, longList, engine);
+
 			if (isFolder) {
 				nrOfFolders++;
 			} else {
@@ -91,8 +87,27 @@ public class Ls extends AbstractCommand implements CommandProvider {
 		engine.println(nrOfFolders + " Folder(s), " + nrOfFiles + " File(s)");
 	}
 
-	protected void listChild(final FileObject base, final FileObject child, boolean isFolder,
-			boolean showRelativePath, Engine engine) throws FileSystemException {
+	private void listChild(final FileObject base, final FileObject child,
+			boolean showRelativePath, boolean isFolder, boolean longList, Engine engine)
+			throws FileSystemException {
+
+		if (longList && longListType != null && longListType.equals("dos")) {
+			SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
+			listChildDosStyle(base, child, isFolder, showRelativePath,
+					dateFormatter, engine);
+		} else if (longList) {
+			SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
+			listChildNixStyle(base, child, isFolder, showRelativePath,
+					dateFormatter, engine);
+		} else {
+			listChild(base, child, isFolder, showRelativePath, engine);
+		}
+				
+	}
+
+	protected void listChild(final FileObject base, final FileObject child,
+			boolean isFolder, boolean showRelativePath, Engine engine)
+			throws FileSystemException {
 
 		if (showRelativePath) {
 			engine.print(base.getName().getRelativeName(child.getName()));
@@ -107,91 +122,101 @@ public class Ls extends AbstractCommand implements CommandProvider {
 
 	}
 
-	protected void listChildDosStyle(final FileObject base, final FileObject child,
-			boolean isFolder, boolean showRelativePath, DateFormat dateFormatter, 
-			Engine engine) throws FileSystemException {
+	protected void listChildDosStyle(final FileObject base,
+			final FileObject child, boolean isFolder, boolean showRelativePath,
+			DateFormat dateFormatter, Engine engine) throws FileSystemException {
 
 		long timestamp = child.getContent().getLastModifiedTime();
 		Date date = new Date(timestamp);
-		
+
 		String middle = "                 ";
 		if (isFolder) {
 			middle = "   <DIR>         ";
-		}
-		else {
+		} else {
 			long size = child.getContent().getSize();
 			String sizeAsString = Long.toString(size);
 			String formattedSize;
-			if (sizeAsString.length()<=3) {
+			if (sizeAsString.length() <= 3) {
 				formattedSize = sizeAsString;
-			}
-			else {
+			} else {
 				int start = sizeAsString.length() % 3;
-				if (start==0) start=3;
-				formattedSize = sizeAsString.substring(0,start);
-			 			
-				for (int i=start; i<sizeAsString.length(); i=i+3) {
-					formattedSize = formattedSize + "." + sizeAsString.substring(i, i+3);
+				if (start == 0)
+					start = 3;
+				formattedSize = sizeAsString.substring(0, start);
+
+				for (int i = start; i < sizeAsString.length(); i = i + 3) {
+					formattedSize = formattedSize + "."
+							+ sizeAsString.substring(i, i + 3);
 				}
 			}
-			middle = middle.substring(0, middle.length() - formattedSize.length()) + formattedSize;
+			middle = middle.substring(0, middle.length()
+					- formattedSize.length())
+					+ formattedSize;
 		}
-				
-		engine.print(dateFormatter.format(date) + middle + " "); 
-		
+
+		engine.print(dateFormatter.format(date) + middle + " ");
+
 		if (showRelativePath) {
 			engine.println(base.getName().getRelativeName(child.getName()));
 		} else {
 			engine.println(child.getName().getBaseName());
-		}		
-		
+		}
+
 	}
-	
-	protected void listChildNixStyle(final FileObject base, final FileObject child,
-			boolean isFolder, boolean showRelativePath, DateFormat dateFormatter, 
-			Engine engine) throws FileSystemException {
+
+	protected void listChildNixStyle(final FileObject base,
+			final FileObject child, boolean isFolder, boolean showRelativePath,
+			DateFormat dateFormatter, Engine engine) throws FileSystemException {
 
 		String start;
-		
+
 		if (isFolder) {
-			start="d";
+			start = "d";
 		} else {
-			start="-";
+			start = "-";
 		}
-		
-		if (child.isWriteable()) start += "w";
-		else start += "-";
-		
-		if (child.isReadable()) start += "r";
-		else start += "-";
-		
+
+		if (child.isWriteable())
+			start += "w";
+		else
+			start += "-";
+
+		if (child.isReadable())
+			start += "r";
+		else
+			start += "-";
+
 		long timestamp = child.getContent().getLastModifiedTime();
 		Date date = new Date(timestamp);
-		
+
 		String middle = "            ";
 		if (!isFolder) {
 			long size = child.getContent().getSize();
 			String sizeAsString = Long.toString(size);
-			middle = middle.substring(0, middle.length() - sizeAsString.length()) + sizeAsString;
+			middle = middle.substring(0, middle.length()
+					- sizeAsString.length())
+					+ sizeAsString;
 		}
-			
+
 		engine.print(start += middle + " " + dateFormatter.format(date) + " ");
-		
+
 		if (showRelativePath) {
 			engine.println(base.getName().getRelativeName(child.getName()));
 		} else {
 			engine.println(child.getName().getBaseName());
-		}		
+		}
 	}
 
 	/**
-	 * Set the format the long listing should use. 
-	 * @param longListType dos for MS-DOS style, nix for Unix/Linux style
+	 * Set the format the long listing should use.
+	 * 
+	 * @param longListType
+	 *            dos for MS-DOS style, nix for Unix/Linux style
 	 */
 	public void setLongListType(String longListType) {
 		this.longListType = longListType;
 	}
-	
+
 	public String getLongListType() {
 		return this.longListType;
 	}
@@ -203,5 +228,5 @@ public class Ls extends AbstractCommand implements CommandProvider {
 	public void setDateFormat(String dateFormat) {
 		this.dateFormat = dateFormat;
 	}
-	
+
 }
