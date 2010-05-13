245dcde664c73338932d1ce248a95f8fe8b5d2e2
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2010-05-13 13:43:45 +0000

    share by default - creating a new manager is expensive
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@194 edfdf9a5-49d8-4bc8-8513-45a678140012

--- utils/src/main/java/org/vfsutils/factory/FileSystemManagerFactory.java
+++ utils/src/main/java/org/vfsutils/factory/FileSystemManagerFactory.java
@@ -9,13 +9,14 @@ import org.apache.commons.vfs.FileSystemManager;
 /**
  * This is a configurable replacement of the default VFS class.
  * You can configure which class to instantiate and whether it
- * should be reused.
+ * should be reused. Creating a new manager is expensive so
+ * sharing it is usually the best option.
  * @author kleij - at - users.sourceforge.net
  * 
  */
 public class FileSystemManagerFactory {
 
-	private boolean share = false;
+	private boolean share = true;
 	private String managerClassName = "org.apache.commons.vfs.impl.StandardFileSystemManager";
 
 	private FileSystemManager sharedManager = null;
@@ -37,7 +38,7 @@ public class FileSystemManagerFactory {
 	}
 
 	/**
-	 * Returns a FileSystemManager. If share is true then a shared instance is
+	 * Returns a FileSystemManager. If share is true (default) then a shared instance is
 	 * returned, else a new one is created every time.
 	 * 
 	 * @return
