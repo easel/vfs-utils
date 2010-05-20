7ab2baac5d137a9165631bc29e8311a075d96e0b
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2010-05-20 11:51:30 +0000

    allow making unique file systems
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@201 edfdf9a5-49d8-4bc8-8513-45a678140012

---
+++ utils/src/main/java/org/vfsutils/session/UniqueFileSystemConfigBuilder.java
@@ -0,0 +1,34 @@
+package org.vfsutils.session;
+import org.apache.commons.vfs.FileSystemConfigBuilder;
+import org.apache.commons.vfs.FileSystemOptions;
+
+
+/**
+ * Allows making the file system options unique so the file system will not 
+ * be shared with other resolvers. You can only safely close the file system
+ * with FileSystemManager.closeFileSystem(FileSystem) if the file system is 
+ * not shared.
+ * @author kleij - at - users.sourceforge.net
+ *
+ */
+public class UniqueFileSystemConfigBuilder extends FileSystemConfigBuilder {
+
+	private static final UniqueFileSystemConfigBuilder INSTANCE = new UniqueFileSystemConfigBuilder();
+	
+	public static UniqueFileSystemConfigBuilder getInstance() {
+		return INSTANCE;
+	}
+	
+	protected Class getConfigClass() {
+		return UniqueFileSystemConfigBuilder.class;
+	}
+	
+	public void setUniqueKey(FileSystemOptions opts) {
+		this.setParam(opts, "uniquekey", new Object());
+	}
+	
+	public Object getUniqueKey(FileSystemOptions opts) {
+		return this.getParam(opts, "uniquekey");
+	}
+
+}
