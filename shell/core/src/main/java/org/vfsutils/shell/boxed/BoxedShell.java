9a07e2ee9eb301c680cb14fd0535bcb670a5515a
kleij <kleij@edfdf9a5-49d8-4bc8-8513-45a678140012>
2010-10-10 20:54:29 +0000

    allow passing options
    git-svn-id: https://vfs-utils.svn.sourceforge.net/svnroot/vfs-utils/trunk@215 edfdf9a5-49d8-4bc8-8513-45a678140012

--- shell/core/src/main/java/org/vfsutils/shell/boxed/BoxedShell.java
+++ shell/core/src/main/java/org/vfsutils/shell/boxed/BoxedShell.java
@@ -2,22 +2,27 @@ package org.vfsutils.shell.boxed;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.VFS;
 import org.vfsutils.shell.Arguments;
 import org.vfsutils.shell.CommandParser;
 import org.vfsutils.shell.MultilineCommandParser;
+import org.vfsutils.shell.Arguments.Option;
+import org.vfsutils.shell.Arguments.OptionMap;
 
 public class BoxedShell extends org.vfsutils.shell.Shell {
 
-	public BoxedShell(InputStream in, String path, boolean askUsername, boolean askPassword, boolean askDomain, boolean virtual) throws FileSystemException {		
+	public BoxedShell(InputStream in, String path, boolean askUsername, boolean askPassword, boolean askDomain, boolean virtual, Map options) throws FileSystemException {		
 		this.engine = new BoxedEngine(this, new BoxedCommandRegistry(), VFS.getManager());
 		this.reader = new InputStreamReader(in);
 		customizeEngine(engine);
 		loadRc();
 		
-		((BoxedEngine) this.engine).setStartDir(path, askUsername, askPassword, askDomain, virtual);
+		((BoxedEngine) this.engine).setStartDir(path, askUsername, askPassword, askDomain, virtual, options);
 	}
 	
 	public static void main(String[] args) {
@@ -31,7 +36,8 @@ public class BoxedShell extends org.vfsutils.shell.Shell {
 					arguments.hasFlag("u"),
 					arguments.hasFlag("p"),
 					arguments.hasFlag("d"),
-					arguments.hasFlag("virtual"));
+					arguments.hasFlag("virtual"),
+					transformOptions(arguments.getOptions()));
 			
 			shell.go();
 		}
@@ -42,5 +48,14 @@ public class BoxedShell extends org.vfsutils.shell.Shell {
 		 
 	}
 
-	
+	protected static Map transformOptions(OptionMap options) {
+		Map result = new TreeMap();
+		Iterator iterator = options.keySet().iterator();
+		while (iterator.hasNext()) {
+			String key = (String) iterator.next();
+			Option option = (Option) options.get(key);
+			result.put(option.getName(), option.getValue());
+		}
+		return result;
+	}
 }
